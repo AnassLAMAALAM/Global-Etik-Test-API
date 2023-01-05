e58c73fbfa54c373c22d10afc8094866217566db
Anass LAMAALAM <anasslam69@gmail.com>
2023-01-05 22:32:06 +0100

    Add Comments to User controller

--- Global.Etik.Api/Controllers/UserController.cs
+++ Global.Etik.Api/Controllers/UserController.cs
@@ -20,21 +20,36 @@ namespace Global.Etik.Api.Controllers
             _mediator = mediator;
         }
 
+        /// <summary>
+        /// Create a new user 
+        /// </summary>
+        /// <param name="create">user model</param>
+        /// <returns>return user id</returns>
         [HttpPost("Create", Name = "AddUser")]
         public async Task<ActionResult<Guid>> Create([FromBody] CreateUserCommand create)
         {
             Guid id = await _mediator.Send(create);
             return Ok(id);
         }
+        /// <summary>
+        /// Update user
+        /// </summary>
+        /// <param name="update">user model</param>
+        /// <returns>return </returns>
         [HttpPut("Update", Name = "UpdateUser")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [ProducesDefaultResponseType]
-        public async Task<ActionResult> valid([FromBody] UpdateUserCommand update)
+        public async Task<ActionResult> Update([FromBody] UpdateUserCommand update)
         {
-            await _mediator.Send(update);
+             await _mediator.Send(update);
             return NoContent();
         }
+        /// <summary>
+        /// Delete a old user 
+        /// </summary>
+        /// <param name="id">id of given user to delete</param>
+        /// <returns></returns>
         [HttpDelete("{id}", Name = "DeleteUser")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
@@ -45,12 +60,21 @@ namespace Global.Etik.Api.Controllers
             await _mediator.Send(delete);
             return NoContent();
         }
+        /// <summary>
+        /// Get users list
+        /// </summary>
+        /// <returns>Users list</returns>
         [HttpGet("GetAll", Name = "ListUsers")]
         public async Task<ActionResult<List<ListUsersMV>>> GetAll()
         {
             var dtos = await _mediator.Send(new ListUsersQuery());
             return Ok(dtos);
         }
+        /// <summary>
+        /// Get a user with given id 
+        /// </summary>
+        /// <param name="id">Given id</param>
+        /// <returns>return user</returns>
         [HttpGet("{id}", Name = "GetById")]
         public async Task<ActionResult<UserMV>> GetById(Guid id)
         {
